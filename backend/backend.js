087fbada0167d3003c0b2d51a28c4c0c30a1db40
arthur <lazzarotto.arthur90@gmail.com>
2022-08-27 10:47:27 +0200

    prenier ripos

---
+++ backend/backend.js
@@ -0,0 +1,7 @@
+const name = 'avion '
+
+const salutation = function (name) {
+    console.log (`Bonjour ${name}, bienvenue en NodeJS`)
+}
+
+salutation(name)
---
+++ frontend/index.js
@@ -0,0 +1,5 @@
+
+const camion = document.querySelector('#roro')
+const avion = document.createElement('h1')
+
+camion.append(avion)
